9e6c2113f6aac2b77beed28929ba2bdf549fd15c
tunetune06 <61481722+tunetune06@users.noreply.github.com>
2020-11-08 10:09:53 +0900

    修正

--- page.php
+++ page.php
@@ -2,14 +2,12 @@
 <?php get_header(); ?>
 
 <main>
-  <div class="section-common">
+  <article class="section-common">
     <?php while(have_posts()) : the_post(); ?>
       <h2 class="section-title"><?php the_title(); ?></h2>
-      <article>
         <?php the_content(); ?>
-      </article>
-    <?php endwhile; ?>
-  </div>
+        <?php endwhile; ?>
+  </article>
 </main>
 
 <?php get_footer(); ?>
