4f337480072b8139de68288aea89eb57ca8022fe
tunetune06 <61481722+tunetune06@users.noreply.github.com>
2020-11-19 13:09:17 +0900

    fv修正

--- front-page.php
+++ front-page.php
@@ -2,8 +2,7 @@
 <?php get_header(); ?>
 <section class="fv">
   <div class="main-copy">
-    <h2 class="fv-text">Shun Watanabe</h2>
-    <p class="fv-sub-text">Welcome to my page</p>
+    <h2 class="fv-text">Shun<br>Watanabe</h2>
   </div>
   <div class="fv-message section-common">
     <p><?php the_content(); ?></p>
